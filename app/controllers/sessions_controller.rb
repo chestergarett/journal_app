add3beb21192bfda8879eb1f1349bb2bcbd77de5
chestergarett <chestergarett@gmail.com>
2021-09-01 01:11:17 +0800

    react UI signup and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = "Successfully logged in."
-      redirect_to user
+      redirect_to tweets_path
     else
       flash.now[:alert] = "There was something wrong with your login details."
       render 'new'
