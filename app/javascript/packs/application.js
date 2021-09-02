3226199fd4e59464d9a1af2ac37a305338f207fc
chestergarett <chestergarett@gmail.com>
2021-09-02 15:35:14 +0800

    react rails fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '../stylesheets/application'
 
-Rails.start()
+Rails.start()   
 // Turbolinks.start()
 ActiveStorage.start()
 // Support component names relative to this directory:
